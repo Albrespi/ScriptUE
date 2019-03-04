3bef865bc878f37b73256916950db03423e1f45a
Victor Albrespy <valbre89@r12.script.univ-paris-diderot.fr>
2019-03-04 15:11:04 +0100

    Nouvelle version

---
+++ code.py
@@ -0,0 +1,69 @@
+#salles = [False,False,False,True,False,False,False,False]
+
+"""
+def print_state(repr3):
+    state_str = ''
+    for element in repr3:
+        if element == True:
+            state_str = state_str + '1'
+        else:
+            state_str = state_str + '-'
+    print(state_str)
+    return
+
+    return
+state = [False,False,False,True,False,False,False,False]
+print_state(repr3=state)
+"""
+
+
+"""
+mois_31 = ["Mars", "Janvier"]
+mois_30 = ["Mai", "Novembre"]
+def nb_jours(Mois):
+    if Mois in mois_31:
+        print(31)
+    else:
+        print (30)
+nb_jours("Mars")
+"""
+"""
+def nombre(x, y):
+
+#EXERCICE CONDITIONS
+
+    if x == y:
+        print("test OK")
+    else:
+        print("test KO")
+nombre(10,10)
+"""
+"""
+def inferieur(x):
+    if x < 10:
+        print(x)
+    else:
+        return
+inferieur(127837291918)
+
+
+def zeroneuf(x):
+
+    for x in range(10):
+        print(x)
+
+zeroneuf(1)
+"""
+"""
+def zeroneuf2():
+    x = 1
+    while x <= 9:
+        print(x)
+        x += 1
+zeroneuf2()
+"""
+def decreme(x):
+    while x > 0:
+        x = x-1
+        print(x)
+decreme(10)
---
+++ snake.py
@@ -0,0 +1,14 @@
+"""œœœœœœœœœœœœœœœdef print_state(repr3):
+    state_str = ''
+    for element in repr3:
+        if element == True:
+            state_str = state_str + '1'
+        else:
+            state_str = state_str + '-'
+    print(state_str)
+    return
+
+    return
+state = [False,False,False,True,False,False,False,False]
+print_state(repr3=state)
+"""
